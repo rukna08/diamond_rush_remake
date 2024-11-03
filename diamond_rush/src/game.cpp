9d983f8c9c973b9eceaa672684c24b15fc093dca
Ankur Banerjee <rukna2003@gmail.com>
2024-11-04 03:20:33 +0530

    made a pink fullscreen window which dies after 3 secs

--- diamond_rush/src/game.cpp
+++ diamond_rush/src/game.cpp
@@ -5,11 +5,28 @@
 
 
 
+SDL_Window* window;
+
+
+
 int main(int argc, char* argv[]) {
+
+
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+
+
+    window = SDL_CreateWindow("Diamond Rush Remake", 0, 0, 1920, 1080, SDL_WINDOW_FULLSCREEN);
     
+    
+    
+    SDL_FillRect(SDL_GetWindowSurface(window), 0, SDL_MapRGB(SDL_GetWindowSurface(window)->format, 255, 0, 255));
+    SDL_UpdateWindowSurface(window);
 
 
-    // Clean slate.
+
+    SDL_Delay(3000);
 
 
 
