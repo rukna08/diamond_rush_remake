ae6200c1b2a0e0548351f5160e1f946605639751
Ankur Banerjee <rukna2003@gmail.com>
2024-11-04 12:54:13 +0530

    created basic game loop with input handling

--- diamond_rush/src/game.cpp
+++ diamond_rush/src/game.cpp
@@ -9,6 +9,13 @@ SDL_Window* window;
 
 
 
+void draw();
+void process_input();
+
+bool is_game_running = true;
+
+
+
 int main(int argc, char* argv[]) {
 
 
@@ -19,18 +26,64 @@ int main(int argc, char* argv[]) {
 
     window = SDL_CreateWindow("Diamond Rush Remake", 0, 0, 1920, 1080, SDL_WINDOW_FULLSCREEN);
     
+    while (is_game_running) {
+
+        draw();
+
+        process_input();
+
+    }
     
+
+
+
+    return 0;
+
+
+
+}
+
+
+
+void draw() {
     
+
+    
+
     SDL_FillRect(SDL_GetWindowSurface(window), 0, SDL_MapRGB(SDL_GetWindowSurface(window)->format, 255, 0, 255));
     SDL_UpdateWindowSurface(window);
 
 
 
-    SDL_Delay(3000);
+}
 
 
 
-    return 0;
+void process_input() {
+    
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        
+
+        switch (event.type) {
+
+
+
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    is_game_running = false;
+                }
+            break;
+
+
+
+        }
+
+
+    }
+
+
 
 
 
