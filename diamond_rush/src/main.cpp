23092b57dadc92e955fecb29546c821aadb3dd53
ansuman-code003 <mazumder.code003@gmail.com>
2024-11-10 12:29:30 +0530

    Added functionallity to draw walls.

--- diamond_rush/src/main.cpp
+++ diamond_rush/src/main.cpp
@@ -1,8 +1,12 @@
 #define SPRITE_SIZE 64
+#define WINDOW_RES_X 1080
+#define WINDOW_RES_Y 720
+
 
 #include <SDL.h>
 #include <SDL_image.h>
 #include <string>
+#include <vector>
 #include "player.h" 
 #include "wall.h" 
 
@@ -12,18 +16,43 @@ Player* player;
 Wall* wall;
 
 bool is_game_running = true;
+const int wallNumber = 10;
+
 
 void draw();
+void draw_wall(int, std::vector<Wall>);
 void process_input();
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(1920, 1080, SDL_WINDOW_FULLSCREEN, &window, &renderer);
+    SDL_CreateWindowAndRenderer(WINDOW_RES_X, WINDOW_RES_Y, SDL_WINDOW_FULLSCREEN, &window, &renderer);
     IMG_Init(IMG_INIT_PNG);
     player = new Player(renderer); 
-    wall = new Wall(renderer);
+    
+    // Creating a array of wall pointer objects 
+    
+    std::vector<Wall> walls;
+    //On Horizontal (TOP) Side
+    for (int i = 0; i <= wallNumber; i++) {
+        walls.emplace_back(SPRITE_SIZE*i,0, renderer);
+    }
+    //On Vertical (LEFT) Side
+    for (int i = 0; i <= wallNumber; i++) {
+        walls.emplace_back(0, SPRITE_SIZE * i, renderer);
+    }
+    //On Horizontal (BOTTOM) Side
+    for (int i = 0; i <= wallNumber; i++) {
+        walls.emplace_back(SPRITE_SIZE * i, WINDOW_RES_Y, renderer);
+    }
+    //On Vertical (LEFT) Side
+    for (int i = 0; i <= wallNumber; i++) {
+        walls.emplace_back(WINDOW_RES_X, SPRITE_SIZE * i, renderer);
+    }
+
+    //Game Loop
     while (is_game_running) {
         draw();
+        draw_wall(wallNumber,walls);
         process_input();
         player->move_constant_right();
         player->move_constant_left();
@@ -40,9 +69,15 @@ int main(int argc, char* argv[]) {
 void draw() {
     SDL_SetRenderDrawColor(renderer, 173, 216, 230, 255);
     SDL_RenderCopy(renderer, player->texture, 0, &player->rect);
-    SDL_RenderCopy(renderer, wall->texture, 0, &wall->rect);
+    //SDL_RenderCopy(renderer, wall->texture, 0, &wall->rect);
     SDL_RenderPresent(renderer);
     SDL_RenderClear(renderer);
+}
+
+void draw_wall(int x,std::vector<Wall> walls) {
+    for (int i = 0; i <= x*4; i++) {
+        SDL_RenderCopy(renderer, walls[i].texture, 0, &walls[i].rect);
+    }
 }
 
 void process_input() {
