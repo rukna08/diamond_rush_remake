5eadd1b864edcdc202466719a084399c6a20d787
ansuman-code003 <mazumder.code003@gmail.com>
2024-11-14 01:48:37 +0530

    Project Clean up.

--- diamond_rush/src/wall.cpp
+++
@@ -1,9 +0,0 @@
-#include "wall.h"
-
-
-Wall::Wall(int x,int y,SDL_Renderer* renderer) {
-	
-	rect = {x,y,SPRITE_SIZE,SPRITE_SIZE};
-	texture = IMG_LoadTexture(renderer, "data/sprite_wall.png");
-}
-
