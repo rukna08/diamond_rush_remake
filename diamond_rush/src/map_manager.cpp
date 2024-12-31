425c9cd9ec3b55e678885796bba258724a66d895
Ankur Banerjee <rukna2003@gmail.com>
2024-12-31 06:31:03 +0530

    level data serialisation (save/load into disk) working for walls

--- diamond_rush/src/map_manager.cpp
+++ diamond_rush/src/map_manager.cpp
@@ -5,9 +5,15 @@ void save_map(std::vector<Wall>& stage_wall, int command) {
     std::ofstream angkor_level_file(ANGKOR_WAT);
     // [OPTIONAL] Delete everything on file before saving
     //angkor_level_file.open(ANGKOR_WAT, std::ofstream::out | std::ofstream::trunc);
+    
+    // we should create a vector of level items
+    // level_items [] = [ walls*, player*, stones*, ... ]  <---- pseudo code btw
+    // then iterate over the the level items and access their type and rect positions
+    // to store the data in the file.
+
     if (stage_wall.size() == 0) return;
     for (int i = 0; i < stage_wall.size(); i++) {
-        angkor_level_file << stage_wall[i].rect.x << " " << stage_wall[i].rect.y << "\n";
+        angkor_level_file << stage_wall[i].type << " " << stage_wall[i].rect.x << " " << stage_wall[i].rect.y << "\n";
     }
     angkor_level_file.close();
 }
@@ -18,9 +24,12 @@ void reload_map(std::vector<Wall>& stage_wall, SDL_Renderer* rndr) {
     std::string line;
     while (std::getline(angkor_level_file, line)) {
         std::istringstream stream(line);
+        std::string level_item_type;
         int x, y;
-        if (stream >> x >> y) {
-            place_wall_pixels(stage_wall,x, y,rndr);
+        if (stream >> level_item_type >> x >> y) {
+            if (level_item_type == "wall") {
+                place_wall_pixels(stage_wall, x, y,rndr);
+            }
         }
     }
     angkor_level_file.close();
--- diamond_rush/src/wall.h
+++ diamond_rush/src/wall.h
@@ -3,11 +3,14 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <string>
+
 
 class Wall {
 public:
 	SDL_Rect rect;
 	SDL_Texture* texture;
+	std::string type;
 
 	Wall(int x,int y, SDL_Renderer* renderer);
 };
